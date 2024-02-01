eed9a42c596f74003444e8814718f849b7bfa9ee
Nando PL <nandopiless@gmail.com>
2024-02-01 17:03:33 +0100

    condition to know if the order file exists

--- src/task1.js
+++ src/task1.js
@@ -21,30 +21,33 @@ if (fs.existsSync(productsPath)) {
             products[order.id] = parseFloat(order.cost); // creates an object 'products' were we have the id linked with its cost
         })
         .on('end', () => {
-            fs.createReadStream(ordersPath)
-                .pipe(csv())
-                .on('data', (order) => {
-                    const orderProducts = order.products.split(' ').map((productId) => parseFloat(productId)); // creates an array where are all the products bought in that order
-                    const orderCost = orderProducts.reduce((totalCost, productId) => totalCost + products[productId], 0); // sum of all product prices
-                    orders.push({ id: order.id, euros: orderCost });
-                })
-                .on('end', () => {
-                    // If the directory doesn't exist it will be created
-                    if (!fs.existsSync(resultFolderPath))
-                        fs.mkdirSync(resultFolderPath, { recursive: true }); // if there are more directories inside it will create all ones
+            if (fs.existsSync(ordersPath)) {
+                fs.createReadStream(ordersPath)
+                    .pipe(csv())
+                    .on('data', (order) => {
+                        const orderProducts = order.products.split(' ').map((productId) => parseFloat(productId)); // creates an array where are all the products bought in that order
+                        const orderCost = orderProducts.reduce((totalCost, productId) => totalCost + products[productId], 0); // sum of all product prices
+                        orders.push({ id: order.id, euros: orderCost });
+                    })
+                    .on('end', () => {
+                        // If the directory doesn't exist it will be created
+                        if (!fs.existsSync(resultFolderPath))
+                            fs.mkdirSync(resultFolderPath, { recursive: true }); // if there are more directories inside it will create all ones
 
-                    // checks if the file exists
-                    const fileExists = fs.existsSync(resultFile);
+                        // checks if the file exists
+                        const fileExists = fs.existsSync(resultFile);
 
-                    const csvWriterOrders = csvWriter({
-                        path: resultFile,
-                        header: [{ id: 'id', title: 'id' }, { id: 'euros', title: 'euros' }],
-                        append: fileExists // If the file exists won't add headers
+                        const csvWriterOrders = csvWriter({
+                            path: resultFile,
+                            header: [{ id: 'id', title: 'id' }, { id: 'euros', title: 'euros' }],
+                            append: fileExists // If the file exists won't add headers
+                        });
+
+                        csvWriterOrders.writeRecords(orders)
+                            .then(() => console.log('[+] Order Prices written successfully'));
                     });
-
-                    csvWriterOrders.writeRecords(orders)
-                        .then(() => console.log('[+] Order Prices written successfully'));
-                });
+            } else
+                console.log(`[-] File ${ordersFile} not found`);
         });
 } else
     console.log(`[-] File ${productsFile} not found`);
